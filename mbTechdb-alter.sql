1f01266a28049a58966063b48282760cc8225d94
Gyana ranjan biswal <98556719+Gyana545@users.noreply.github.com>
2024-07-18 16:33:01 +0530

    Update mbTechdb-alter.sql

--- mbTechdb-alter.sql
+++ mbTechdb-alter.sql
@@ -1,1 +1,4 @@
-
+USE `EXP1`;
+ALTER TABLE employee DROP COLOUM IF EXIST salary age ;
+ALTER TABLE employee ADD salary int age int;
+UPDATE employee SET salary = "1000" WHERE id = 1 age = 25 WHERE id = 1;
